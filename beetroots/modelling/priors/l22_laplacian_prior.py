b8f1077184d4d9a5734ba2f33f3801f3a83c4ae8
Nicolas Goeman <nicolas.goemn@gmail.com>
2024-05-27 14:10:10 +0200

    Update: l22 prior neglog_pdf to deal with mtm or not.

--- beetroots/modelling/priors/l22_laplacian_prior.py
+++ beetroots/modelling/priors/l22_laplacian_prior.py
@@ -12,42 +12,8 @@ from beetroots.modelling.priors.abstract_spatial_prior import SpatialPrior
 
 
 @decorator_nb()
-def compute_laplacian(Var: xp.ndarray, list_edges: xp.ndarray) -> xp.ndarray:
-    r"""evaluates the image Laplacian for each of the D maps, :math:`\Delta \Theta_{\cdot d}`
-
-    Parameters
-    ----------
-    Var : xp.ndarray of shape (N, D)
-        D vectors of N pixels
-    list_edges : xp.ndarray
-        set of edges in the graph induced by the spatial regularization
-
-    Returns
-    -------
-    xp.ndarray of shape (N, D)
-        image Laplacian for each of the D maps
-    """
-    laplacian_ = xp.zeros_like(Var)
-    N = laplacian_.shape[0]
-
-    for i in range(N):
-        mask_i_m = list_edges[:, 1] == i
-        mask_i_p = list_edges[:, 0] == i
-
-        laplacian_[i] += xp.sum(
-            (Var[list_edges[mask_i_p, 1], :] - Var[list_edges[mask_i_p, 0], :]),
-            axis=0,
-        )
-        laplacian_[i] -= xp.sum(
-            (Var[list_edges[mask_i_m, 1], :] - Var[list_edges[mask_i_m, 0], :]),
-            axis=0,
-        )
-    return laplacian_  # (N, D)
-
-
-@decorator_nb()
 def compute_laplacian_local(
-    Var: xp.ndarray, n: int, list_edges: xp.ndarray, list_pixel_candidates: xp.ndarray
+    Var: xp.ndarray, idx_pix: xp.ndarray, list_edges: xp.ndarray, k_mtm: int
 ) -> xp.ndarray:
     """computes the local laplacian only for the one modified pixel
 
@@ -68,24 +34,33 @@ def compute_laplacian_local(
         the laplacian of the candidates
     """
     # D = Var.shape[1]
-    N_candidates = list_pixel_candidates.shape[0]
-    laplacian_ = xp.zeros_like(list_pixel_candidates)
+    laplacian_ = xp.zeros((idx_pix.shape[0], k_mtm, *Var.shape[2:]))  if k_mtm>0 else xp.zeros((idx_pix.shape[0], *Var.shape[1:])) # (n_pix, k_mtm, D) or (n_pix, D)
 
-    mask_i_m = list_edges[:, 1] == n
-    mask_i_p = list_edges[:, 0] == n
 
-    for i in range(N_candidates):
-        Var[n] = list_pixel_candidates[i] * 1
+    for i, n in enumerate(idx_pix):
+        mask_i_m = list_edges[:, 1] == n # No duplicate edges so some edges store the nth pixel as the first element and some as the second element
+        mask_i_p = list_edges[:, 0] == n
 
-        laplacian_[i] += xp.sum(
-            (Var[list_edges[mask_i_p, 1], :] - Var[list_edges[mask_i_p, 0], :]),
-            axis=0,
-        )  # (D,)
-        laplacian_[i] -= xp.sum(
-            (Var[list_edges[mask_i_m, 1], :] - Var[list_edges[mask_i_m, 0], :]),
-            axis=0,
-        )  # (D,)
-    return laplacian_  # (N_candidates, D)
+        if k_mtm == 0:
+            laplacian_[i] += xp.sum(
+                    (Var[list_edges[mask_i_p, 1]] - Var[list_edges[mask_i_p, 0]]),
+                    axis=0,
+                )  # (k_mtm, D,)
+            laplacian_[i] -= xp.sum(
+                (Var[list_edges[mask_i_m, 1]] - Var[list_edges[mask_i_m, 0]]),
+                axis=0,
+            )  # (k_mtm, D,)
+    else: 
+            for j in range(k_mtm):
+                laplacian_[i, j] += xp.sum(
+                    (Var[list_edges[mask_i_p, 1], j] - Var[list_edges[mask_i_p, 0], j]),
+                    axis=0,
+                )  # (k_mtm, D,)
+                laplacian_[i, j] -= xp.sum(
+                    (Var[list_edges[mask_i_m, 1], j] - Var[list_edges[mask_i_m, 0], j]),
+                    axis=0,
+                )  # (k_mtm, D,)
+    return laplacian_  # (n_pix, k_mtm, D) or (n_pix, D)
 
 
 @decorator_nb()
@@ -116,54 +91,6 @@ def compute_gradient_from_laplacian(
     return g  # (N, D)
 
 
-@decorator_nb()
-def _neglog_pdf_one_pix(
-    Var: xp.ndarray,
-    idx_pix: xp.ndarray,
-    list_pixel_candidates: xp.ndarray,
-    list_edges: xp.ndarray,
-) -> xp.ndarray:
-    """computes the neg log-prior when only one pixel is modified
-
-    Parameters
-    ----------
-    Var : xp.ndarray of shape (N, D)
-        current iterate
-
-    idx_pix : xp.ndarray
-        array of the indices of the pixels to consider (:math:`0 \leq n \leq N - 1`)
-
-    list_pixel_candidates : xp.ndarray of shape (N_candidates, D)
-        the list of all candidates for pixel n
-
-    Returns
-    -------
-    xp.ndarray of shape (N_candidates,)
-        the leg log-prior of the candidates
-    """
-    n_pix, k_mtm, D = list_pixel_candidates.shape # FIXME: not consistent with the doc above.
-    neglog_p = xp.zeros((n_pix, k_mtm, D))
-    i = 0
-    n_previous = -500_000
-
-    for n in idx_pix:
-        if n != n_previous:
-            list_edges_pix = list_edges[
-                (list_edges[:, 0] == n) | (list_edges[:, 1] == n)
-            ]
-
-        if list_edges_pix.size > 0:
-            laplacian_ = compute_laplacian_local(
-                Var, n, list_edges_pix, list_pixel_candidates[i]
-            )  # (k_mtm, D)
-            neglog_p[i, :, :] += laplacian_**2  # (n_pix, k_mtm, D)
-
-        i += 1
-        n_previous = n * 1
-
-    return neglog_p
-
-
 class L22LaplacianSpatialPrior(SpatialPrior):
     r"""L22 smooth spatial prior, valid for both 1D and 2D tensors. Its pdf is defined as
 
@@ -181,73 +108,46 @@ class L22LaplacianSpatialPrior(SpatialPrior):
         with_weights: bool = True,
         pixelwise: bool = False,
     ) -> xp.ndarray:
-        assert Var.shape == (self.N, self.D)
+        assert Var.shape[0] == self.N
+        assert Var.shape[-1]==self.D
+        k_mtm = Var.shape[1] if len(Var.shape) == 3 else 0
+
+        n_pix = len(idx_pix) if idx_pix is not None else self.N
 
         if pixelwise:
-            neglog_p = xp.zeros((self.N, self.D))
+            neglog_p = xp.zeros((n_pix, self.D)) if k_mtm == 0 else xp.zeros((n_pix, k_mtm, self.D))
         else:
-            neglog_p = xp.zeros((self.D,))
+            neglog_p = xp.zeros((self.D)) if k_mtm == 0 else xp.zeros((k_mtm, self.D))
 
         if self.list_edges.size > 0:
-            laplacian_ = compute_laplacian(Var, self.list_edges)
+            laplacian_ = compute_laplacian_local(Var, idx_pix, self.list_edges, k_mtm)
             if pixelwise:
                 neglog_p += laplacian_**2  # (N,D)
             else:
-                neglog_p += xp.sum(laplacian_**2, axis=0)  # (D,)
+                neglog_p += xp.sum(laplacian_**2, axis=0)  # (D,) or (k_mtm, D)
 
         if with_weights:
             if pixelwise:
-                neglog_p *= self.weights[None, :]
+                if k_mtm == 0:
+                    neglog_p *= self.weights[None, :]
+                else:
+                    neglog_p *= self.weights[None, None, :]
             else:
-                neglog_p *= self.weights
+                if k_mtm == 0:
+                    neglog_p *= self.weights
+                else:
+                    neglog_p *= self.weights[None, :]
 
         # neglog_p /= self.N * self.D
-        return neglog_p  # (D,) if not pixelwise or (N, D) if pixelwise
+        return neglog_p  # (n_pix, k_mtm, D,) or (n_pix, D) or (D,) or (k_mtm, D) depending on the values of pixelwise and k_mtm
 
-    def neglog_pdf_one_pix(
-        self,
-        Var: xp.ndarray,
-        idx_pix: xp.ndarray,
-        list_pixel_candidates: xp.ndarray,
-        other_weights: Optional[xp.ndarray] = None,
-    ) -> xp.ndarray:
-        """
-        computes the neg log-prior when only one pixel is modified
-
-        Parameters
-        ----------
-        Var : xp.ndarray of shape (N, D)
-            current iterate
-
-        idx_pix : xp.ndarray
-            array of the indices of the pixels to consider (:math:`0 \leq n \leq N - 1`)
-
-        list_pixel_candidates : xp.ndarray of shape (N_candidates, D)
-            the list of all candidates for pixel n
-
-        Returns
-        -------
-        xp.ndarray of shape (N_candidates,)
-            the neg log-prior of the candidates
-        """
-        neglog_p = _neglog_pdf_one_pix(
-            Var, idx_pix, list_pixel_candidates, self.list_edges
-        )  # # (n_pix, k_mtm, D)
-        # neglog_p /= self.N * self.D
-
-        if other_weights is None:
-            return xp.sum(
-                neglog_p * self.weights[None, None, :], axis=2
-            )  # (n_pix, k_mtm)
-        else:
-            return xp.sum(
-                neglog_p * other_weights[None, None, :], axis=2
-            )  # (n_pix, k_mtm)
 
     def gradient_neglog_pdf(self, Var: xp.ndarray) -> xp.ndarray:
-        assert Var.shape == (self.N, self.D)
+        assert Var.shape[0] == self.N
+        assert Var.shape[-1]==self.D
+        k_mtm = Var.shape[1] if len(Var.shape) == 3 else 0
 
-        laplacian_ = compute_laplacian(Var, self.list_edges)
+        laplacian_ = compute_laplacian_local(Var, Var.shape[0], self.list_edges, k_mtm)
 
         g = compute_gradient_from_laplacian(laplacian_, self.list_edges)  # (N, D)
         # g /= self.N * self.D
